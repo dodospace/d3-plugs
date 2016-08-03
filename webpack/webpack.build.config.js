58d04aa06d32dd89e5be07a53768f7fc94cf4732
顾少华 <gush@uyunsoft.cn>
2016-08-03 18:59:25 +0800

    webpack config

---
+++ webpack/webpack.build.config.js
@@ -0,0 +1,47 @@
+var _ = require('lodash');
+var path = require('path');
+var webpack = require('webpack');
+var Clean = require('clean-webpack-plugin');
+var baseConfig = require('./base.config')
+var scripts = require('./scripts');
+
+
+var config = _.merge(baseConfig, {
+    entry: _.merge({
+            bundle: './demo/src/main.jsx'
+        },
+        scripts.chunks),
+    output: {
+        path: path.resolve(__dirname, '../demo/web/build'),
+        publicPath: 'build/',
+        filename: '[name].js',
+        chunkFilename: 'chunk.[id].js',
+        pathinfo: true
+    },
+    devtool: 'cheap-module-source-map ',
+    plugins: [
+        new Clean('../demo/web/build'),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            minimize: true,
+            sourceMap: false,
+            exclude: [
+                /node_modules/,
+                /bower_components/
+            ],
+            output: {
+                comments: false,
+                ascii_only: true
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ].concat(baseConfig.plugins)
+});
+
+module.exports = config;
